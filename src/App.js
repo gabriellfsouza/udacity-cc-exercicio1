053f22a05e60884a36066e2286db76e7a301460a
root <gabriellfsouza@gmail.com>
2019-02-05 00:09:35 +0000

    final

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+  	text:""
+  }
+  
+  updateText(texto){
+    debugger;
+  	this.setState({
+  		text:texto
+  	});
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,8 +22,8 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Say Something" />
-          <p className="echo">Echo:</p>
+          <input type="text" placeholder="Say Something" value={this.state.text} onChange={(e)=>this.updateText(e.target.value)}/>
+          <p className="echo">Echo:{this.state.text}</p>
           <p>This should mirror the text you typed into the input field.</p>
         </div>
       </div>
